c1c72157f7173b3dce49dfe07e01ea0d0e87fdcb
NabilaNurFadhilah <nurfhadilahn@gmail.com>
2024-12-31 09:01:26 +0700

    models.py

---
+++ config/db_config.py
@@ -0,0 +1,11 @@
+from flask_mysqldb import MySQL
+from flask import Flask
+
+def init_db(app: Flask):
+    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = ''
+    app.config['MYSQL_DB'] = 'pwpnabila221'
+
+    mysql = MySQL(app)
+    return mysql
